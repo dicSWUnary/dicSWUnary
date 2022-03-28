aa2fb3467d656207379e484f71e3bb0ae719292d
JubyKim <uon9816@naver.com>
2022-03-29 00:06:53 +0900

    connect to ViewController

--- dicSWUnary/dicSWUnary/VerificationViewController.swift
+++ dicSWUnary/dicSWUnary/VerificationViewController.swift
@@ -12,41 +12,43 @@ class VerificationViewController: UIViewController {
     let dictionaryImage = UIImageView().then{
         $0.image = UIImage(named: "dictionaryImage")
     }
-
     
-    let emailTitle = UILabel().then{
-        $0.text = "웹메일"
-//        $0.font = UIFont.systemFont(ofSize: 20.0)
-    }
     let emailTextField = UITextField().then{
         $0.allowsEditingTextAttributes = true
-        $0.borderStyle = .roundedRect
-//            .textFieldStyle(RoundedBorderTextFieldStyle())
-//        $0.backgroundColor = .black
+        $0.borderStyle = .none
+        $0.attributedPlaceholder = NSAttributedString(
+            string: "웹 메일 주소를 입력하세요.",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray]
+        )
+    }
+    let underbar = UIView().then{
+        $0.backgroundColor = .gray
     }
     
     let emailCaution = UILabel().then{
-        $0.text = "서울여자대학교 웹메일로만 인증이 가능합니다."
+        $0.text = "⚠️서울여자대학교 웹메일로만 인증이 가능합니다."
         $0.font = UIFont.systemFont(ofSize: 13.0)
         $0.textColor = .lightGray
     }
     
     let sendButton = UIButton().then{
-//        $0.titleLabel.text = "sibal"
+        $0.setBackgroundImage(UIImage(named: "buttonBackground"), for: .normal)
         $0.setTitle("인증 링크 받기", for: .normal)
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 10.0)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 15.0)
         $0.setTitleColor(.black, for: .normal)
         $0.backgroundColor = .white
     }
-    
-    let verifyNumberTitle = UILabel().then{
-        $0.text = "입력하신 주소의 메일함을 확인해주세요."
+
+    let checkLabel = UILabel().then{
+        $0.text = "💌입력하신 주소의 메일함을 확인해주세요."
+        $0.textColor = .darkGray
     }
     
     let verifyButton = UIButton().then{
-        $0.setTitle("인증하기", for: .normal)
+        $0.setBackgroundImage(UIImage(named: "buttonBackground"), for: .normal)
+        $0.setTitle("인증완료", for: .normal)
         $0.sizeToFit()
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 10.0)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 15.0)
         $0.setTitleColor(.black, for: .normal)
         $0.backgroundColor = .white
         $0.addTarget(self, action: #selector(verifyButtonTapped), for: .touchUpInside)
@@ -56,7 +58,7 @@ class VerificationViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        self.view.backgroundColor = .yellow
+        self.view.backgroundColor = .white
         loadComponents(me: self.view)
         allLayout()
     }
@@ -64,70 +66,73 @@ class VerificationViewController: UIViewController {
     
     @objc func verifyButtonTapped(){
         print("toNextView")
-        let childVC = MissionViewController()
+        let childVC = ViewController()
         childVC.modalPresentationStyle = .fullScreen
         self.present(childVC, animated: true, completion: nil)
     }
     
     func loadComponents(me : UIView){
+        
+        me.addSubview(dictionaryImage)
+        
         //email Component
-        me.addSubview(emailTitle)
+//        me.addSubview(emailTitle)
         me.addSubview(emailTextField)
+        me.addSubview(underbar)
         me.addSubview(emailCaution)
         
         me.addSubview(sendButton)
         
-        me.addSubview(verifyNumberTitle)
+        me.addSubview(checkLabel)
         me.addSubview(verifyButton)
     }
     
     func allLayout(){
         emailLayout()
-        verifyLayout()
     }
     
     func emailLayout(){
-        
-        emailTitle.snp.makeConstraints{
-            $0.centerY.equalToSuperview().offset(-50)
-            $0.leading.equalToSuperview().offset(30)
-
-        }
-        emailTextField.snp.makeConstraints{
-            $0.top.equalTo(emailTitle.snp.bottom).offset(8)
-            $0.leading.equalToSuperview().offset(30)
+        dictionaryImage.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().offset(-200)
+//            $0.top.equalToSuperview().offset(50)
         }
         
         emailCaution.snp.makeConstraints{
-            $0.top.equalTo(emailTextField.snp.bottom).offset(8)
-            $0.leading.equalTo(emailTextField.snp.leading).offset(10)
+            $0.top.equalTo(dictionaryImage.snp.bottom).offset(50)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+        }
+        
+        emailTextField.snp.makeConstraints{
+            $0.top.equalTo(emailCaution.snp.bottom).offset(8)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+        }
+        underbar.snp.makeConstraints{
+            $0.top.equalTo(emailTextField.snp.bottom).offset(3)
+            $0.leading.equalTo(emailTextField)
+            $0.trailing.equalTo(emailTextField)
+            $0.height.equalTo(1)
         }
         
         sendButton.snp.makeConstraints{
-            $0.centerY.equalTo(emailTextField.snp.centerY)
-            $0.leading.equalTo(emailTextField.snp.trailing).offset(10)
+            $0.top.equalTo(emailTextField.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(30)
             $0.trailing.equalToSuperview().offset(-30)
             $0.width.equalTo(100)
         }
-    }
-    
-    func verifyLayout(){
-        
-        verifyNumberTitle.snp.makeConstraints{
-            $0.top.equalTo(emailCaution.snp.bottom).offset(20)
-            $0.leading.equalTo(emailTitle)
+        checkLabel.snp.makeConstraints{
+            $0.top.equalTo(sendButton.snp.bottom).offset(25)
+            $0.centerX.equalToSuperview()
         }
         
         verifyButton.snp.makeConstraints{
-            $0.top.equalTo(verifyNumberTitle.snp.bottom).offset(20)
+            $0.top.equalTo(checkLabel.snp.bottom).offset(16)
             $0.leading.equalToSuperview().offset(30)
             $0.trailing.equalToSuperview().offset(-30)
-//            $0.width.equalTo(100)
+            $0.width.equalTo(100)
+            
         }
-        
-        
-        
     }
-
-
 }
