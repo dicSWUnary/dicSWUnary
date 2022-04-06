7a6b95031848bce4252a4c49b6565965194a3071
JubyKim <uon9816@naver.com>
2022-04-06 20:08:00 +0900

    fix

---
+++ dicSWUnary/dicSWUnary/myButton.swift
@@ -0,0 +1,30 @@
+//
+//  myButton.swift
+//  dicSWUnary
+//
+//  Created by 이규빈 on 2022/03/29.
+//
+
+import Foundation
+import UIKit
+
+class myButton: UIButton {
+
+    override init(frame: CGRect) {
+            super.init(frame: frame)
+            setupView()
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            setupView()
+        }
+
+        func setupView() {
+            layer.borderWidth = 1
+            layer.borderColor = UIColor.darkGray.cgColor
+            layer.cornerRadius = 5
+            clipsToBounds = true
+            
+        }
+}
