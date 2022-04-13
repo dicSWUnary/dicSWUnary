89a1cdbd0478ea0016008d7a51a39815b7ef2552
‘9yubean2’ <mi701170@naver.com>
2022-04-13 18:00:22 +0900

    VUpgrade View Controller

--- dicSWUnary/dicSWUnary/ViewController.swift
+++ dicSWUnary/dicSWUnary/ViewController.swift
@@ -15,8 +15,9 @@ import Then
 
 class ViewController: UIViewController{
     var backgroundImage = UIImageView().then{
-        $0.image = UIImage(named: "background_paper")
+        $0.image = UIImage(named: "ViewControllerBackground")
     }
+    
     
     var initData = [missions(advise: "정문에 위치한 서울여대의 마크! 졸업 사진 스팟이에요!", building_name: "정문", floor: "건물 외부", guide_image: "guide0", hint: "정문을 통해서 50주년으로 가볼까요?", index: 0, location_image: "location_0", spot_name: "학교 마크", succes_check: true),
                   missions(advise: "토익, 등본, 발표자료 등 프린트가 필요하다면 카피웍스를 이용해보세요.", building_name: "50주년 기념관", floor: "지하 1층", guide_image: "guide1", hint: "계단 옆 연못 옆을 볼까요?", index: 1, location_image: "location_1", spot_name: "카피웍스", succes_check: true),
@@ -29,45 +30,23 @@ class ViewController: UIViewController{
     var dbData = [missions]()
     var completeList = [missions]()
     var completeCheck = [Int]()
-    var now = Int()
+    var now = 0
     
-    //헤더 뷰
-    let headerView = UIView().then{
-        $0.backgroundColor = .white
-    }
-    
-    let welcomeLevel = UILabel().then{
-        $0.textColor = .black
-        $0.text = "삐약삐약 새내기 🐥"
-        $0.font = UIFont.systemFont(ofSize: 30.0, weight: .bold)
-    }
-    let welcomeName = UILabel().then{
-        $0.text = "슈니, 안녕하세요!"
-        $0.font = UIFont.systemFont(ofSize: 30.0, weight: .bold)
-    }
-    
+
+
     //MARK: -- Profile
-    let profileImage = UIImageView().then{
-        $0.image = UIImage(named: "swunie")
-    }
-    
-    let profileName = UILabel().then{
-        $0.text = "은빈"
-        $0.font = UIFont.systemFont(ofSize: 24.0, weight: .semibold)
-    }
-    
-    let profileStatus = UILabel().then{
-        $0.text = "새내기"
-        $0.font = UIFont.systemFont(ofSize: 18.0, weight: .semibold)
-        $0.textColor = UIColor.lightGray
+    let welcomeName = UILabel().then{
+        $0.text = ">>슈니, 반가워요!🐥"
+        $0.font = UIFont(name: "NeoDunggeunmoCode-Regular", size: 35)
+        $0.textColor = .white
     }
     
     
-    let goToMissionButton = myButton().then{
-        $0.setTitle("미션하러 가기", for: .normal)
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18.0)
-        $0.setTitleColor(.darkGray, for: .normal)
-        $0.backgroundColor = .white
+    let goToMissionButton = UIButton().then{
+        $0.setTitle(">>미션하러 가기", for: .normal)
+        $0.titleLabel?.font = UIFont(name: "NeoDunggeunmoCode-Regular", size: 35)
+        $0.setTitleColor(UIColor(named: "vcYellow"), for: .normal)
+        
         $0.addTarget(self, action: #selector(MissionBtnTapped), for: .touchUpInside)
     }
     
@@ -76,27 +55,31 @@ class ViewController: UIViewController{
     //재학상태
     
     let statusTitle = UILabel().then{
-        $0.textColor = .black
-        $0.text = "재학 상태"
-        $0.font = UIFont.systemFont(ofSize: 18.0, weight: .semibold)
+        $0.textColor = .white
+        $0.text = ">>재학 상태"
+        $0.font = UIFont(name: "NeoDunggeunmoCode-Regular", size: 20)
+    }
+    
+    var statusImage = UIImageView().then{
+        $0.image = UIImage(named: "group6")
     }
     
     let statusView = UIView().then{
-        $0.backgroundColor = .white
+        $0.backgroundColor = .black
         $0.layer.cornerRadius = 4
     }
     let nowLevel = UILabel().then{
         $0.text = "🐥\n새내기"
-        $0.textColor = .black
-        $0.font = UIFont.systemFont(ofSize: 16.0)
+        $0.textColor = .white
+        $0.font = UIFont(name: "NeoDunggeunmoCode-Regular", size: 18)
         $0.lineBreakMode = .byWordWrapping
         $0.numberOfLines = 0
         $0.textAlignment = .center
     }
     let nextLevel = UILabel().then{
         $0.text = "🎓\n학사"
-        $0.textColor = .black
-        $0.font = UIFont.systemFont(ofSize: 16.0)
+        $0.textColor = .white
+        $0.font = UIFont(name: "NeoDunggeunmoCode-Regular", size: 18)
         $0.lineBreakMode = .byWordWrapping
         $0.numberOfLines = 0
         $0.textAlignment = .center
@@ -118,26 +101,28 @@ class ViewController: UIViewController{
     }
     let totalMission = UILabel().then{
         $0.text = "/8"
-        $0.textColor = .black
-        $0.font = UIFont.systemFont(ofSize: 12.0)
+        $0.textColor = .white
+        $0.font = UIFont(name: "NeoDunggeunmoCode-Regular", size: 14)
     }
     
     
     //MARK: -- Activity
     let activityTitle = UILabel().then{
-        $0.textColor = .black
-        
-        $0.text = "대외 활동"
-        $0.font = UIFont.systemFont(ofSize: 18.0, weight: .semibold)
+        $0.textColor = .white
+        $0.text = ">>대외 활동"
+        $0.font = UIFont(name: "NeoDunggeunmoCode-Regular", size: 20)
     }
     let activityView = UIView().then{
-        $0.backgroundColor = .white
+        $0.backgroundColor = .black
         $0.layer.cornerRadius = 4
     }
+    let activityImage = UIImageView().then{
+        $0.image = UIImage(named: "group8")
+    }
     let activityKind = UILabel().then{
-        $0.textColor = .black
-        $0.text = "🔥 완료한 미션"
-        $0.font = UIFont.systemFont(ofSize: 14.0, weight: .medium)
+        $0.textColor = .white
+        $0.text = "🔥완료한 미션"
+        $0.font = UIFont(name: "NeoDunggeunmoCode-Regular", size: 18)
     }
     
     //collection view
@@ -148,57 +133,22 @@ class ViewController: UIViewController{
         let  cv = UICollectionView(frame: .zero, collectionViewLayout: flowlayout)
         flowlayout.scrollDirection = .horizontal
         cv.isScrollEnabled = true
+        cv.backgroundColor = .black
         return cv
     }()
     
-    //MARK: -- Footer
-    let footerView = UIView().then{
-        $0.backgroundColor = .white
-    }
-    
-    let logoutButton = myButton().then{
-        $0.setTitle("로그아웃", for: .normal)
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18.0)
-        $0.setTitleColor(.darkGray, for: .normal)
-        $0.backgroundColor = .white
-    }
-    
-    let exitButton = UIButton().then{
-        $0.setTitleColor(.darkGray, for: .normal)
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18.0)
-        $0.setTitle("회원탈퇴", for: .normal)
-        $0.setUnderline()
-    }
     
     //navi : 여기서 MissionViewController()으로 이동
     
 
     let defaults = UserDefaults.standard
-    
-    override func viewDidAppear(_ animated: Bool) {
-        var tempCompleteCheck = [Int]()
-//        dbData = CoreDataManager.shared.retrieveData()
-        getAllMission()
-        
-        //makeList
-        self.completeList = dbData.filter{$0.succes_check == true}
-        
-        for i in completeList{
-            tempCompleteCheck.append(i.index)
-        }
-        completeCheck = tempCompleteCheck
-        now = completeCheck.max()! + 1
-        determineProgress()
-        completeMissionCollectionView.reloadData()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         checkFirstOrnot()
         dbData = initData
         getAllMission()
         navigationItem.title = ""
-        view.backgroundColor = UIColor(named: "graybackground")
+        view.backgroundColor = .white
         
         //makeList
         self.completeList = dbData.filter{$0.succes_check == true}
@@ -207,33 +157,30 @@ class ViewController: UIViewController{
             completeCheck.append(i.index)
         }
         now = completeCheck.max()! + 1
-        if now == 8 {
-            let childVC = CertificationViewController()
-            childVC.modalPresentationStyle = .fullScreen
-            self.present(childVC, animated: false, completion: nil)
-        }
         determineProgress()
         view.addSubview(backgroundImage)
-        view.addSubview(headerView)
-        headerView.addSubview(welcomeLevel)
-        headerView.addSubview(welcomeName)
-        
-        view.addSubview(profileImage)
-        view.addSubview(profileName)
-        view.addSubview(profileStatus)
+
+        view.addSubview(welcomeName)
         view.addSubview(goToMissionButton)
-        view.addSubview(statusTitle)
+        
+        
+        
         view.addSubview(statusView)
+        view.addSubview(statusImage)
+        view.addSubview(statusTitle)
         statusView.addSubview(nowLevel)
         statusView.addSubview(statusProgress)
         statusView.addSubview(succesMission)
         statusView.addSubview(totalMission)
         statusView.addSubview(nextLevel)
         
-        activityView.addSubview(activityKind)
-        activityView.addSubview(completeMissionCollectionView)
         view.addSubview(activityTitle)
+        activityView.addSubview(completeMissionCollectionView)
         view.addSubview(activityView)
+        activityView.addSubview(activityImage)
+        activityView.addSubview(activityKind)
+        
+        
         
         //collection view 권한 부여
         self.completeMissionCollectionView.dataSource = self
@@ -241,9 +188,7 @@ class ViewController: UIViewController{
         self.completeMissionCollectionView.translatesAutoresizingMaskIntoConstraints = false
         self.completeMissionCollectionView.register(ActivityCell.self, forCellWithReuseIdentifier: cellID)
         
-        view.addSubview(footerView)
-        footerView.addSubview(logoutButton)
-        footerView.addSubview(exitButton)
+
         headerLayout()
         mainLayout()
         statusLayout()
@@ -256,7 +201,6 @@ class ViewController: UIViewController{
         if defaults.bool(forKey: "First Launch") == true {
             print("Second+")
             defaults.set(true, forKey: "FirstLaunch")
-//            CoreDataManager.shared.retrieveData()
         } else {
 
             for i in initData{
@@ -285,64 +229,39 @@ class ViewController: UIViewController{
     }
     func headerLayout(){
         backgroundImage.snp.makeConstraints{
-            $0.top.bottom.leading.trailing.equalToSuperview()
+            $0.top.equalToSuperview().offset(40)
+            $0.bottom.equalToSuperview().offset(0)
+            $0.leading.trailing.equalToSuperview()
         }
-        headerView.snp.makeConstraints{
-            $0.top.equalToSuperview()
-            $0.leading.equalToSuperview()
-            $0.trailing.equalToSuperview()
-            $0.height.equalToSuperview().multipliedBy(1.0 / 4.5)
-        }
-        
-        welcomeLevel.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(20)
-            $0.bottom.equalTo(welcomeName.snp.top).offset(-5)
-        }
-        
-        welcomeName.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(20)
-            $0.bottom.equalToSuperview().offset(-25)
-        }
+
     }
     
     func mainLayout(){
-        profileImage.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(20)
-            $0.top.equalTo(headerView.snp.bottom).offset(20)
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            
-        }
-        profileName.snp.makeConstraints{
-            $0.centerY.equalTo(profileImage).offset(-10)
-            $0.leading.equalTo(profileImage.snp.trailing).offset(20)
-            
-        }
-        profileStatus.snp.makeConstraints{
-            $0.top.equalTo(profileName.snp.bottom).offset(10)
-            $0.leading.equalTo(profileImage.snp.trailing).offset(20)
-        }
-        goToMissionButton.snp.makeConstraints{
-            $0.top.equalTo(profileImage.snp.bottom).offset(20)
-            $0.leading.equalToSuperview().offset(20)
-            $0.trailing.equalToSuperview().offset(-20)
+        welcomeName.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(130)
             $0.centerX.equalToSuperview()
         }
-        
     }
     
     func statusLayout(){
         
         statusTitle.snp.makeConstraints{
-            $0.top.equalTo(goToMissionButton.snp.bottom).offset(20)
-            $0.leading.equalToSuperview().offset(10)
+            $0.top.equalTo(welcomeName.snp.bottom).offset(45)
+            $0.leading.equalToSuperview().offset(25)
+        }
+        
+        statusImage.snp.makeConstraints{
+            $0.top.equalTo(welcomeName.snp.bottom).offset(40)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.height.equalToSuperview().multipliedBy(1.0 / 7.0)
         }
         
         statusView.snp.makeConstraints{
             $0.top.equalTo(statusTitle.snp.bottom).offset(10)
-            $0.leading.equalToSuperview().offset(10)
-            $0.trailing.equalToSuperview().offset(-10)
-            $0.height.equalToSuperview().multipliedBy(1.0 / 13.0)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.height.equalToSuperview().multipliedBy(1.0 / 10.0)
         }
         nowLevel.snp.makeConstraints{
             $0.centerY.equalToSuperview()
@@ -350,10 +269,10 @@ class ViewController: UIViewController{
             
         }
         statusProgress.snp.makeConstraints{
-            $0.top.equalToSuperview().offset(15)
+            $0.centerY.equalToSuperview()
             $0.leading.equalTo(nowLevel.snp.trailing).offset(10)
             $0.trailing.equalTo(nextLevel.snp.leading).offset(-10)
-            $0.height.equalToSuperview().multipliedBy(1.0/3.5)
+            $0.height.equalToSuperview().multipliedBy(1.0/5.0)
             
             
         }
@@ -375,50 +294,44 @@ class ViewController: UIViewController{
     
     func activityLayout(){
         activityTitle.snp.makeConstraints{
-            $0.top.equalTo(statusView.snp.bottom).offset(20)
-            $0.leading.equalToSuperview().offset(10)
+            $0.top.equalTo(statusView.snp.bottom).offset(40)
+            $0.leading.equalToSuperview().offset(20)
+        }
+        
+        activityImage.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(0)
+            $0.leading.equalToSuperview().offset(5)
+            $0.bottom.equalToSuperview().offset(0)
+            $0.trailing.equalToSuperview().offset(-5)
+            
         }
         activityView.snp.makeConstraints{
             $0.top.equalTo(activityTitle.snp.bottom).offset(10)
             $0.leading.equalToSuperview().offset(10)
             $0.trailing.equalToSuperview().offset(-10)
-            $0.height.equalToSuperview().multipliedBy(1.0 / 7.0)
+            $0.height.equalToSuperview().multipliedBy(1.0 / 5.0)
         }
         activityKind.snp.makeConstraints{
-            $0.top.equalToSuperview().offset(10)
-            $0.leading.equalToSuperview().offset(10)
+            $0.top.equalToSuperview().offset(20)
+            $0.leading.equalToSuperview().offset(25)
         }
         
         completeMissionCollectionView.snp.makeConstraints{
-            $0.top.equalTo(activityKind.snp.bottom).offset(10)
-            $0.leading.equalToSuperview().offset(10)
-            $0.bottom.equalToSuperview().offset(-10)
-            $0.trailing.equalToSuperview().offset(-10)
+            $0.top.equalTo(activityKind.snp.bottom).offset(0)
+            $0.leading.equalToSuperview().offset(25)
+            $0.bottom.equalToSuperview().offset(-20)
+            $0.trailing.equalToSuperview().offset(-25)
         }
         
     }
     
     
     func footerLayout(){
-        
-        footerView.snp.makeConstraints{
-            $0.bottom.equalToSuperview()
-            $0.leading.equalToSuperview()
-            $0.trailing.equalToSuperview()
-            $0.height.equalTo(view.snp.height).multipliedBy(1.0 / 6.5)
-        }
-        
-        logoutButton.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(70)
-            $0.trailing.equalToSuperview().offset(-70)
-            $0.top.equalToSuperview().offset(20)
-            $0.centerX.equalToSuperview()
-            
-        }
-        exitButton.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(40)
-            $0.trailing.equalToSuperview().offset(-40)
-            $0.top.equalTo(logoutButton.snp.bottom).offset(10)
+
+        goToMissionButton.snp.makeConstraints{
+            $0.top.equalTo(activityView.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
             $0.centerX.equalToSuperview()
         }
     }
@@ -461,7 +374,6 @@ extension ViewController: UICollectionViewDataSource {
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! ActivityCell
-        //        cell.backgroundColor = .red
         if (completeCheck.contains(indexPath.row)) {
             cell.backgroundView = UIImageView(image: UIImage(named: String(format: "guideImage%d", indexPath.row)))
         }
