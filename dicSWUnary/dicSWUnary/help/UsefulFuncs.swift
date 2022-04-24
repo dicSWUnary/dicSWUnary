185c6236b151bf2018b48e15ecc84bd15450f74c
JubyKim <uon9816@naver.com>
2022-04-25 00:54:35 +0900

    button enablity depends on isValid

--- dicSWUnary/dicSWUnary/help/UsefulFuncs.swift
+++ dicSWUnary/dicSWUnary/help/UsefulFuncs.swift
@@ -51,3 +51,23 @@ extension UIView {
         }
     }
 }
+
+extension UIApplication {
+    class func getTopMostViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return getTopMostViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return getTopMostViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return getTopMostViewController(base: presented)
+        }
+        return base
+    }
+}
+
+
+
